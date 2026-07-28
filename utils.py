27955939d4f9dd8775b92c6fb53876f22d730fff
bitsureteddy-blip <bitsureteddy@gmail.com>
2026-07-29 01:56:34 +0200

    Description

--- utils.py
+++ utils.py
@@ -48,3 +48,9 @@ def escape_markdown(text: str) -> str:
     """Escapes characters that have special meaning in Telegram Markdown."""
     return str(text).replace("_", "\\_").replace("*", "\\*").replace("`", "\\`").replace("[", "\\[")
 
+
+def cache_key(*args) -> str:
+    """Génère une clé de cache unique"""
+    raw = "|".join(str(a) for a in args)
+    return hashlib.md5(raw.encode()).hexdigest()
+
